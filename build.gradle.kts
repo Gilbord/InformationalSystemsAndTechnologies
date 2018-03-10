e0d0f9da32f9c7987f7c9ec1f0eed8e4fac59867
Daniil <burhaunt@gmail.com>
2018-03-10 18:02:20 +0300

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+group = "gilbord"
+version = "1.0"
+
+buildscript {
+    var kotlin_version: String by extra
+    kotlin_version = "1.2.21"
+
+    repositories {
+        mavenCentral()
+    }
+    
+    dependencies {
+        classpath(kotlinModule("gradle-plugin", kotlin_version))
+    }
+    
+}
+
+apply {
+    plugin("kotlin")
+}
+
+val kotlin_version: String by extra
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile(kotlinModule("stdlib-jdk8", kotlin_version))
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
